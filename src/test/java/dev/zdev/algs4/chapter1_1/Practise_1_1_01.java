7f350dc6aaac81d46a16430302af7a54207ce066
Zhai Dev <29057533+zdevcs@users.noreply.github.com>
2021-01-28 20:53:58 +0800

    Create maven.yml

---
+++ src/test/java/dev/zdev/algs4/chapter1_1/Practise_1_1_01.java
@@ -0,0 +1,12 @@
+package dev.zdev.algs4.chapter1_1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Practise_1_1_01 {
+    @Test
+    public void name() {
+        assertTrue(true);
+    }
+}
